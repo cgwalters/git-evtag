d4ad3fd225bbec76700a2fcb98035d349ea4c3ce
Colin Walters <walters@verbum.org>
2015-06-27 21:07:02 -0400

    Actually checksum objects now, and support printing stats

--- src/git-evtag.c
+++ src/git-evtag.c
@@ -25,9 +25,11 @@
 #include <string.h>
 
 static gboolean opt_print_only = TRUE;
+static gboolean opt_verbose = FALSE;
 
 static GOptionEntry option_entries[] = {
   { "print-only", 0, 0, G_OPTION_ARG_NONE, &opt_print_only, "Don't create a tag, just compute and print evtag data", NULL },
+  { "verbose", 'v', 0, G_OPTION_ARG_NONE, &opt_verbose, "Print statistics on what we're hashing", NULL },
   { NULL }
 };
 
@@ -47,57 +49,114 @@ handle_libgit_ret (int r, GError **error)
   return FALSE;
 }
 
+struct EvTag {
+  GChecksum *checksum;
+  git_repository *repo;
+  git_odb *db;
+  guint n_commits;
+  guint n_trees;
+  guint n_blobs;
+  guint64 total_object_size;
+};
+
 static void
-hash_object (GChecksum      *checksum,
-             git_odb_object *object)
+checksum_odb_object (struct EvTag  *self,
+                     git_odb_object *object)
 {
-  g_checksum_update (checksum, git_odb_object_data (object), git_odb_object_size (object));
+  size_t size = git_odb_object_size (object);
+
+  g_checksum_update (self->checksum, git_odb_object_data (object), size);
+
+  switch (git_odb_object_type (object))
+    {
+    case GIT_OBJ_BLOB:
+      self->n_blobs++;
+      break;
+    case GIT_OBJ_COMMIT:
+      self->n_commits++;
+      break;
+    case GIT_OBJ_TREE:
+      self->n_trees++;
+      break;
+    default:
+      g_assert_not_reached ();
+    }
+
+  self->total_object_size += size;
+}
+
+static gboolean
+checksum_object_id (struct EvTag  *self,
+                    const git_oid *oid,
+                    GError       **error)
+{
+  gboolean ret = FALSE;
+  int r;
+  git_odb_object *odbobj = NULL;
+
+  r = git_odb_read (&odbobj, self->db, oid);
+  if (!handle_libgit_ret (r, error))
+    goto out;
+
+  checksum_odb_object (self, odbobj);
+  
+  ret = TRUE;
+ out:
+  if (odbobj)
+    git_odb_object_free (odbobj);
+  return ret;
 }
 
 struct TreeWalkData {
-  gboolean ret;
+  gboolean caught_error;
+  struct EvTag *evtag;
   GCancellable *cancellable;
   GError **error;
 };
 
 static int
-print_tree_callback (const char *root,
-                     const git_tree_entry *entry,
-                     void *data)
+checksum_tree_callback (const char *root,
+                        const git_tree_entry *entry,
+                        void *data)
 {
-  // struct TreeWalkData *twdata = data;
-  char buf[GIT_OID_HEXSZ+1];
+  struct TreeWalkData *twdata = data;
 
-  git_oid_tostr (buf, sizeof (buf), git_tree_entry_id (entry));
-  g_print ("%s.%s\n", buf, git_object_type2string (git_tree_entry_type (entry)));
+  if (!checksum_object_id (twdata->evtag, git_tree_entry_id (entry),
+                           twdata->error))
+    twdata->caught_error = TRUE;
   return 0;
 }
 
 static gboolean
-print_commit_objects (git_commit *commit, GCancellable *cancellable, GError **error)
+checksum_commit_contents (struct EvTag *self,
+                          git_commit *commit,
+                          GCancellable *cancellable, GError **error)
 {
   gboolean ret = FALSE;
   int r;
   git_tree *tree = NULL;
-  char buf[GIT_OID_HEXSZ+1];
-  struct TreeWalkData twdata = { FALSE, cancellable, error };
+  struct TreeWalkData twdata = { FALSE, self, cancellable, error };
 
-  git_oid_tostr (buf, sizeof (buf), git_object_id((git_object*)commit));
-  g_print ("%s.commit\n", buf);
+  if (!checksum_object_id (self, git_object_id((git_object*)commit), error))
+    goto out;
 
   r = git_commit_tree (&tree, commit);
   if (!handle_libgit_ret (r, error))
     goto out;
 
-  git_oid_tostr (buf, sizeof (buf), git_object_id((git_object*)tree));
-  g_print ("%s.tree\n", buf);
-
-  r = git_tree_walk (tree, GIT_TREEWALK_PRE, print_tree_callback, &twdata);
-  if (!handle_libgit_ret (r, error))
+  if (!checksum_object_id (self, git_object_id((git_object*)tree), error))
     goto out;
 
-  ret = twdata.ret;
+  r = git_tree_walk (tree, GIT_TREEWALK_PRE, checksum_tree_callback, &twdata);
+  if (!handle_libgit_ret (r, error))
+    goto out;
+  if (twdata.caught_error)
+    goto out;
+
+  ret = TRUE;
  out:
+  if (tree)
+    git_tree_free (tree);
   return ret;
 }
 
@@ -110,13 +169,11 @@ main (int    argc,
   GCancellable *cancellable = NULL;
   git_repository *repo = NULL;
   git_object *obj = NULL;
-  git_odb *odb = NULL;
-  git_odb_object *odbobj = NULL;
   git_commit *commit = NULL;
   GOptionContext *context;
   int r;
   const char *rev;
-  GChecksum *checksum = g_checksum_new (G_CHECKSUM_SHA512);
+  struct EvTag self = { NULL, };
   
   /* avoid gvfs (http://bugzilla.gnome.org/show_bug.cgi?id=526454) */
   g_setenv ("GIO_USE_VFS", "local", TRUE);
@@ -136,11 +193,17 @@ main (int    argc,
     }
   rev = argv[1];
 
+  self.checksum = g_checksum_new (G_CHECKSUM_SHA512); 
+
   r = git_repository_open (&repo, ".");
   if (!handle_libgit_ret (r, error))
     goto out;
 
   r = git_revparse_single (&obj, repo, rev);
+  if (!handle_libgit_ret (r, error))
+    goto out;
+
+  r = git_repository_odb (&self.db, repo);
   if (!handle_libgit_ret (r, error))
     goto out;
 
@@ -154,23 +217,30 @@ main (int    argc,
 
   commit = (git_commit*)obj;
 
-  if (!print_commit_objects (commit, cancellable, error))
+  if (!checksum_commit_contents (&self, commit, cancellable, error))
     goto out;
-
-  r = git_repository_odb (&odb, repo);
-  if (!handle_libgit_ret (r, error))
-    goto out;
-  r = git_odb_read (&odbobj, odb, git_object_id (obj));
-  if (!handle_libgit_ret (r, error))
-    goto out;
+  
+  if (opt_print_only)
+    {
+      if (opt_verbose)
+        g_print ("# Git-EVTag-Stats: commits=%u trees=%u blobs=%u bytes=%" G_GUINT64_FORMAT "\n",
+                 self.n_commits,
+                 self.n_trees,
+                 self.n_blobs,
+                 self.total_object_size);
+        
+      g_print ("Git-EVTag-Contents-SHA512: %s\n", g_checksum_get_string (self.checksum));
+    }
+  else
+    g_assert_not_reached ();
 
  out:
   if (repo)
     git_repository_free (repo);
-  if (odbobj)
-    git_odb_object_free (odbobj);
-  if (odb)
-    git_odb_free (odb);
+  if (self.db)
+    git_odb_free (self.db);
+  if (self.checksum)
+    g_checksum_free (self.checksum);
   if (local_error)
     {
       g_printerr ("%s\n", local_error->message);
