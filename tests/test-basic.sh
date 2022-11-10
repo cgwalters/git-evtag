dfb48531f5f769f56e96974fe1cfc5adec567cf7
Simon McVittie <smcv@debian.org>
2022-11-10 18:32:58 +0000

    tests: Configure git to allow file:/// submodules
    Git forbids file:/// for submodules by default, to avoid untrusted
    file:/// repositories being able to break a security boundary
    (CVE-2022-39253). However, this breaks git-evtag's test suite.
    
    In this test suite, all the repositories are under our control and
    we trust them, so bypass that restriction.
    
    Bug-Debian: https://bugs.debian.org/1023803
    Signed-off-by: Simon McVittie <smcv@debian.org>

--- tests/test-basic.sh
+++ tests/test-basic.sh
@@ -30,7 +30,7 @@ echo "ok setup"
 
 git clone repos/coolproject
 cd coolproject
-git submodule update --init
+trusted_git_submodule update --init
 with_editor_script git evtag sign -u 472CDAFA v2015.1
 git show refs/tags/v2015.1 > tag.txt
 TAG='Git-EVTag-v0-SHA512: 58e9834248c054f844f00148a030876f77eb85daa3caa15a20f3061f181403bae7b7e497fca199d25833b984c60f3202b16ebe0ed3a36e6b82f33618d75c569d'
@@ -51,7 +51,7 @@ cd ${test_tmpdir}
 rm coolproject -rf
 git clone repos/coolproject
 cd coolproject
-git submodule update --init
+trusted_git_submodule update --init
 echo 'super cool' > src/cool.c
 if with_editor_script git evtag sign -u 472CDAFA v2015.1 2>err.txt; then
     assert_not_reached "expected failure due to dirty tree"
@@ -69,7 +69,7 @@ cd ${test_tmpdir}
 rm coolproject -rf
 git clone repos/coolproject
 cd coolproject
-git submodule update --init
+trusted_git_submodule update --init
 with_editor_script git evtag sign -u 472CDAFA v2015.1
 git checkout -q HEAD^
 if git evtag verify v2015.1 2>err.txt; then
@@ -82,7 +82,7 @@ cd ${test_tmpdir}
 rm coolproject -rf
 git clone repos/coolproject
 cd coolproject
-git submodule update --init
+trusted_git_submodule update --init
 with_editor_script git evtag sign --with-legacy-archive-tag -u 472CDAFA v2015.1
 git show refs/tags/v2015.1 > tag.txt
 assert_file_has_content tag.txt 'ExtendedVerify-SHA256-archive-tar: 83991ee23a027d97ad1e06432ad87c6685e02eac38706e7fbfe6e5e781939dab'
@@ -98,7 +98,7 @@ cd ${test_tmpdir}
 rm coolproject -rf
 git clone repos/coolproject
 cd coolproject
-git submodule update --init
+trusted_git_submodule update --init
 with_editor_script git evtag sign --no-signature v2015.1
 git show refs/tags/v2015.1 > tag.txt
 assert_file_has_content tag.txt 'Git-EVTag-v0-SHA512: 58e9834248c054f844f00148a030876f77eb85daa3caa15a20f3061f181403bae7b7e497fca199d25833b984c60f3202b16ebe0ed3a36e6b82f33618d75c569d'
@@ -116,7 +116,7 @@ cd ${test_tmpdir}
 rm coolproject -rf
 git clone repos/coolproject2
 cd coolproject2
-git submodule update --init
+trusted_git_submodule update --init
 with_editor_script git evtag sign -u 472CDAFA v2015.1
 git show refs/tags/v2015.1 > tag.txt
 TAG='Git-EVTag-v0-SHA512: d683c087ffee370996b9514f892ec8911451b33e3dfe48ac233a3dca1d63676802236e2d152803f553aa7d05c00f6e30b83f1f73dae9d524378845be02da4b97'
